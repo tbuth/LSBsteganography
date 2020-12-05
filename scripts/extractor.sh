4fdc3cd2acae23e7cd180cc34bf39c44df7ac58f
Tyler <tbuth@uoguelph.ca>
2020-12-04 19:58:44 -0500

    adding comments to extractor script

--- scripts/extractor.sh
+++ scripts/extractor.sh
@@ -1,7 +1,7 @@
-./decoder $1 $2
-cp decodedMsg.txt decodedMessage.c
-gcc decodedMessage.c -o payload
-./payload
-rm decodedMessage.c
+./decoder $1 $2 # run the decoder by passing through CL arguments as input
+cp decodedMsg.txt decodedMessage.c # copy the decoded message to a new file with the .c extension to allow compilation
+gcc decodedMessage.c -o payload # compile the decoded message
+./payload # execute the decoded message
+rm decodedMessage.c #remove the intermediary files
 rm payload
 rm decodedMessage.txt
